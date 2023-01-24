c2ad4773f7e75d36fb2865e24e8acd8a1c911624
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2023-01-24 17:05:50 +0100

    build(uml): create class and use case diagramme

--- project/back_end/src/main/java/com/cvtheme/back_end/entities/UserEntity.java
+++ project/back_end/src/main/java/com/cvtheme/back_end/entities/UserEntity.java
@@ -6,8 +6,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@Inheritance(strategy = InheritanceType.JOINED)
-@Entity(name = "user")
+@MappedSuperclass
 public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
